27221609b8c3db6fbf3d4a55b5a5352a9f8a745e
Chris Rault <chris@smokeyfro.com>
2020-04-22 11:36:34 +0200

    Added home bg

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="absolute top-0 bottom-0 -mb-10 left-0 right-0 z-0 w-full h-screen bg-black">
+  <div class="absolute top-0 bottom-0 -mb-10 left-0 right-0 z-0 w-full h-screen hero">
     <div class="p-10">
-        <div class="flex items-center justify-center h-screen w-full">
+        <div class="flex items-center justify-center h-screen w-full relative z-50">
             <h2 class="text-white text-6xl lg:w-1/2 mx-auto text-center -mt-40">Fast-track your <b>JAM</b>stack development &amp; learning</h2>
         </div>
     </div>
@@ -16,5 +16,22 @@ export default {
 </script>
 
 <style>
-
+body.home {
+  background-image: url(/bg.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+.hero:after {
+  content: '';
+  display: block;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0,0,0,0.9);
+}
 </style>
