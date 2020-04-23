1c8b9397c31241a746f478fd4334957a47cbf278
Travis Reynolds <travis@cirruscreative.co.uk>
2020-04-23 15:01:04 +0000

    Format files with eslint

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,107 +1,135 @@
 <template>
-    <div class="form pb-40" refs>
-        <div class="success" v-if="status === 'success'">
-            <div class="flex items-center justify-start w-full">
-				<div class="w-full p-10 mt-10 bg-gray-100 border-l-8 bg-accent border-accent">
-					<h2>Message sent!</h2>
-					<p class="m-0">Expect a response within the next 24 hours (though usually less).</p>
-				</div>
-			</div>
+  <div
+    class="form pb-40"
+    refs>
+    <div
+      v-if="status === 'success'"
+      class="success">
+      <div class="flex items-center justify-start w-full">
+        <div class="w-full p-10 mt-10 bg-gray-100 border-l-8 bg-accent border-accent">
+          <h2>Message sent!</h2>
+          <p class="m-0">
+            Expect a response within the next 24 hours (though usually less).
+          </p>
         </div>
-        <div class="error" v-if="status === 'error'">
-            Oops, something went wrong. Please try again.
-        </div>
-        <form v-if="!status" @submit="sendForm">
-            <label class="block">
-                <span class="text-gray-700" for="message">Message</span>
-                    <ClientOnly>
-                        <vue-expand
-                        v-model="message" 
-                        :handler="handler" 
-                        placeholder="Write your message here"
-                        min-row="5"
-                        v-focus />
-                    </ClientOnly>
-            </label>
-            <div>
-                <div class="flex flex-col md:flex-row">
-                    <div class="md:w-1/2 md:mr-5">
-                        <label class="block mt-4">
-                            <span class="text-gray-700">Your name</span>
-                            <input name="name" id="name" v-model="name" class="block w-full mt-1 form-input" placeholder="Evan You" required>
-                        </label>
-                    </div>
-                    <div class="md:w-1/2">            
-                        <label class="block mt-4">
-                            <span class="text-gray-700">Your email</span>
-                            <input name="email" id="email" v-model="email" class="block w-full mt-1 form-input" placeholder="evan@vuejs.org" required>
-                        </label>
-                    </div>
-                </div>
-                <input type="hidden" name="_gotcha">
-                <button type="submit" class="block w-full px-10 py-6 mt-4 mb-5 text-xl font-bold bg-gray-800 rounded-md text-accent md:mt-8">
-                    <span>Send it, Sparky!</span>
-                </button>
-            </div>
-        </form>
+      </div>
     </div>
+    <div
+      v-if="status === 'error'"
+      class="error">
+      Oops, something went wrong. Please try again.
+    </div>
+    <form
+      v-if="!status"
+      @submit="sendForm">
+      <label class="block">
+        <span
+          class="text-gray-700"
+          for="message">Message</span>
+        <ClientOnly>
+          <vue-expand
+            v-model="message"
+            v-focus
+            :handler="handler"
+            placeholder="Write your message here"
+            min-row="5" />
+        </ClientOnly>
+      </label>
+      <div>
+        <div class="flex flex-col md:flex-row">
+          <div class="md:w-1/2 md:mr-5">
+            <label class="block mt-4">
+              <span class="text-gray-700">Your name</span>
+              <input
+                id="name"
+                v-model="name"
+                name="name"
+                class="block w-full mt-1 form-input"
+                placeholder="Evan You"
+                required>
+            </label>
+          </div>
+          <div class="md:w-1/2">
+            <label class="block mt-4">
+              <span class="text-gray-700">Your email</span>
+              <input
+                id="email"
+                v-model="email"
+                name="email"
+                class="block w-full mt-1 form-input"
+                placeholder="evan@vuejs.org"
+                required>
+            </label>
+          </div>
+        </div>
+        <input
+          type="hidden"
+          name="_gotcha">
+        <button
+          type="submit"
+          class="block w-full px-10 py-6 mt-4 mb-5 text-xl font-bold bg-gray-800 rounded-md text-accent md:mt-8">
+          <span>Send it, Sparky!</span>
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-    import Vue from 'vue';
+import Vue from 'vue'
 
-    export default {
-        name: 'Form',
-        components: {
-            VueExpand: () => import("vue-expand") 
-        },
-        data: function() {
-            return {
-                handler: new Vue(),
-                status: null,
-                name: null,
-                email: null,
-                budget: '',
-                website: '',
-                message: null,
-                radioValue: "message",
-                message_type: null,
-                message_type: this.radioValue
-            }
-        },
-        mounted(){
-            this.handler.$emit('focus')
-        },
-        methods: {
-            sendForm: function(event) {
-                event.preventDefault()
-
-                fetch('https://formcarry.com/s/iqNMdsHQ9Zt', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Accept: 'application/json'
-                },
-                body: JSON.stringify({ 
-                    name: this.name, 
-                    email: this.email, 
-                    message: this.message,
-                    budget: this.budget, 
-                    website: this.website
-                 })
-                })
-                .then(response => response.json())
-                .then(response => {
-                    if (response.code === 200) {
-                        this.status = 'success'
-                    } else {
-                        // Formcarry error
-                        this.status = 'error'
-                    }
-                })
-                // network error
-                .catch(() => (this.status = 'error'))
-            }
-        }
+export default {
+  name: 'Form',
+  components: {
+    VueExpand: () => import('vue-expand')
+  },
+  data: function () {
+    return {
+      handler: new Vue(),
+      status: null,
+      name: null,
+      email: null,
+      budget: '',
+      website: '',
+      message: null,
+      radioValue: 'message',
+      message_type: null,
+      message_type: this.radioValue
     }
+  },
+  mounted () {
+    this.handler.$emit('focus')
+  },
+  methods: {
+    sendForm: function (event) {
+      event.preventDefault()
+
+      fetch('https://formcarry.com/s/iqNMdsHQ9Zt', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+        body: JSON.stringify({
+          name: this.name,
+          email: this.email,
+          message: this.message,
+          budget: this.budget,
+          website: this.website
+        })
+      })
+        .then(response => response.json())
+        .then(response => {
+          if (response.code === 200) {
+            this.status = 'success'
+          } else {
+            // Formcarry error
+            this.status = 'error'
+          }
+        })
+      // network error
+        .catch(() => (this.status = 'error'))
+    }
+  }
+}
 </script>
--- src/pages/blog/Index.vue
+++ src/pages/blog/Index.vue
@@ -1,11 +1,26 @@
 <template>
   <Layout>
     <div class="h-screen w-1/2 mx-auto">
-      <h1 class="title">Blog</h1>
-      <p class="excerpt">Get the latest updates about what we're thinking about, working on and more.</p>
-      <article v-for="edge in $page.blog.edges" :key="edge.node.id" class="mt-6 mb-5">
-          <h2 class="text-4xl font-bold mb-4 bg-black inline text-white p-3"><g-link :to="`${edge.node.path}`" class="text-white">{{ edge.node.title }}</g-link></h2>
-          <p class="mb-3 text-xl mt-5">{{ edge.node.excerpt }}</p>
+      <h1 class="title">
+        Blog
+      </h1>
+      <p class="excerpt">
+        Get the latest updates about what we're thinking about, working on and more.
+      </p>
+      <article
+        v-for="edge in $page.blog.edges"
+        :key="edge.node.id"
+        class="mt-6 mb-5">
+        <h2 class="text-4xl font-bold mb-4 bg-black inline text-white p-3">
+          <g-link
+            :to="`${edge.node.path}`"
+            class="text-white">
+            {{ edge.node.title }}
+          </g-link>
+        </h2>
+        <p class="mb-3 text-xl mt-5">
+          {{ edge.node.excerpt }}
+        </p>
       </article>
     </div>
   </Layout>
@@ -19,8 +34,8 @@ export default {
     // Hero
   },
   metaInfo: {
-    title: "Blog",
-	description: "Updates from JammeryHQ"
+    title: 'Blog',
+    description: 'Updates from JammeryHQ'
   }
 }
 </script>
