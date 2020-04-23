b8bc8fa7390d984529f85458213db86065ead795
Travis Reynolds <travis@cirruscreative.co.uk>
2020-04-23 15:41:35 +0000

    Add basic method to post to function

--- src/components/Newsletter.vue
+++ src/components/Newsletter.vue
@@ -3,3 +3,35 @@
     <!-- Newsletter -->
   </div>
 </template>
+
+<script>
+// Packages
+import ky from 'ky'
+
+export default {
+  name: 'NewsletterSignup',
+  data: () => ({
+    subscriber: {},
+    sent: false,
+    error: false,
+    loading: false
+  }),
+  methods: {
+    async subscribe () {
+      this.loading = true
+      this.error = false
+
+      try {
+        const subscriberPayload = this.subscriber
+        const { status, message } = await ky.post('/api/newsletterSubscribe', { json: subscriberPayload }).json()
+        this.loading = false
+
+        if (status === 'error') throw new Error(message)
+        this.sent = true
+      } catch (error) {
+        this.error = error.message
+      }
+    }
+  }
+}
+</script>
