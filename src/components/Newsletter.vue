b805940e29110189ece4cb5d13e9cd1c68b20712
Travis Reynolds <travis@cirruscreative.co.uk>
2020-04-23 16:00:10 +0000

    Remove unecessary div

--- src/components/Newsletter.vue
+++ src/components/Newsletter.vue
@@ -2,46 +2,44 @@
   <form
     class="form"
     @submit.prevent="subscribe">
-      <div class="flex flex-col md:flex-row">
-        <div class="md:w-1/2 md:mr-5">
-          <label
-            for="name"
-            class="block mt-4">
-            <span class="text-gray-700">Your name</span>
-            <input
-              id="name"
-              v-model="subscriber.name"
-              name="name"
-              class="block w-full mt-1 form-input"
-              placeholder="Evan You"
-              required>
-          </label>
-        </div>
-        <div class="md:w-1/2">
-          <label
-            for="email"
-            class="block mt-4">
-            <span class="text-gray-700">Your email</span>
-            <input
-              id="email"
-              v-model="subscriber.email"
-              name="email"
-              class="block w-full mt-1 form-input"
-              placeholder="evan@vuejs.org"
-              required>
-          </label>
-        </div>
+    <div class="flex flex-col md:flex-row">
+      <div class="md:w-1/2 md:mr-5">
+        <label
+          for="name"
+          class="block mt-4">
+          <span class="text-gray-700">Your name</span>
+          <input
+            id="name"
+            v-model="subscriber.name"
+            name="name"
+            class="block w-full mt-1 form-input"
+            placeholder="Evan You"
+            required>
+        </label>
       </div>
-      <input
-        type="hidden"
-        name="_gotcha">
-      <button
-        type="submit"
-        class="block w-full px-10 py-6 mt-4 mb-5 text-xl font-bold bg-gray-800 rounded-md text-accent md:mt-8">
-        <span>Send it, Sparky!</span>
-      </button>
-    <div>
+      <div class="md:w-1/2">
+        <label
+          for="email"
+          class="block mt-4">
+          <span class="text-gray-700">Your email</span>
+          <input
+            id="email"
+            v-model="subscriber.email"
+            name="email"
+            class="block w-full mt-1 form-input"
+            placeholder="evan@vuejs.org"
+            required>
+        </label>
+      </div>
     </div>
+    <input
+      type="hidden"
+      name="_gotcha">
+    <button
+      type="submit"
+      class="block w-full px-10 py-6 mt-4 mb-5 text-xl font-bold bg-gray-800 rounded-md text-accent md:mt-8">
+      <span>Send it, Sparky!</span>
+    </button>
   </form>
 </template>
 
