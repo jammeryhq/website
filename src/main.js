ae4b5ddf68648b4d586d9146f8cb75e2e12c475d
Travis Reynolds <travis@cirruscreative.co.uk>
2020-04-24 11:40:28 +0000

    Add segment js for analytics

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import DefaultLayout from '~/layouts/Default.vue'
 // Styles
 import '~/main.css'
 
-export default function (Vue, { router, head, isClient }) {
+export default function (Vue) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
