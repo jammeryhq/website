fbc0714d9f12c93ff8f9a168af4d50ad68835f38
Travis Reynolds <travis@cirruscreative.co.uk>
2020-04-29 20:44:38 +0000

    Add reCaptcha

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 // Layouts
 import DefaultLayout from '~/layouts/Default.vue'
 
+// Packages
+import { VueReCaptcha } from 'vue-recaptcha-v3'
+
 // Styles
 import '~/main.css'
 
 export default function (Vue) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
+
+  // Use Plugins
+  Vue.use(VueReCaptcha, { siteKey: process.env.GRIDSOME_RECAPTCHA_SITE_KEY, loaderOptions: { autoHideBadge: true } })
 }
