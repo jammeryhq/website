706bbd704b3d39fa555a6523bce24c222965da3e
Chris Rault <chris@smokeyfro.com>
2020-05-21 14:49:54 +0200

    Updated the single starter layout

--- src/templates/Starter.vue
+++ src/templates/Starter.vue
@@ -2,14 +2,14 @@
   <Layout>
     <div class="p-6 lg:p-0 w-full md:w-3/4 lg:w-2/3 xl:w-1/2 mx-auto relative">
       <h1 class="title">
-        {{ $page.post.title }}
+        {{ $page.starter.title }}
       </h1>
       <p class="excerpt">
-        {{ $page.post.excerpt }}
+        {{ $page.starter.excerpt }}
       </p>
       <div
         class="content"
-        v-html="$page.post.content" />
+        v-html="$page.starter.content" />
       <div class="comment-form">
         <Comments />
       </div>
@@ -24,20 +24,20 @@ import Comments from '@/components/Comments'
 export default {
   metaInfo () {
     return {
-      title: this.$page.post.title,
+      title: this.$page.starter.title,
       meta: [
         {
           key: 'description',
           name: 'description',
-          content: this.$page.post.excerpt
+          content: this.$page.starter.excerpt
         },
         { property: 'og:type', content: 'article' },
-        { property: 'og:title', content: this.$page.post.title },
-        { property: 'og:description', content: this.$page.post.excerpt },
-        { property: 'og:url', content: this.postUrl },
-        { property: 'article:published_time', content: this.$page.post.date },
-        { name: 'twitter:title', content: this.$page.post.title },
-        { name: 'twitter:description', content: this.$page.post.excerpt },
+        { property: 'og:title', content: this.$page.starter.title },
+        { property: 'og:description', content: this.$page.starter.excerpt },
+        { property: 'og:url', content: this.starterUrl },
+        { property: 'article:published_time', content: this.$page.starter.date },
+        { name: 'twitter:title', content: this.$page.starter.title },
+        { name: 'twitter:description', content: this.$page.starter.excerpt },
         { name: 'twitter:site', content: '@jammeryhq' },
         { name: 'twitter:creator', content: '@jammeryhq' }
       ],
@@ -58,13 +58,16 @@ export default {
 </script>
 
 <page-query>
-query Post ($path: String) {
-  post(path: $path) {
+query Starter ($path: String) {
+  starter(path: $path) {
     title
     slug
     id
     excerpt
     content
+    featured
+    published
+    demo
   }
   metadata {
     siteUrl
