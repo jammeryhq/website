19ec63984243a88571e2f349dd8c59f24d9934f9
Travis Reynolds <travis@cirruscreative.co.uk>
2020-05-21 16:20:52 +0000

    Add default user query

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -9,13 +9,36 @@
 </template>
 
 <script>
+// Components
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+
+// Packages
+import gql from 'graphql-tag'
 
 export default {
   components: {
     Header,
     Footer
+  },
+  apollo: {
+    user: {
+      query: gql`query User ($id: String!) {
+        user (id: $id) {
+          id
+          displayName
+          gravatar
+        }
+      }`,
+      skip () {
+        return this.$auth.loading || !this.$auth.isAuthenticated
+      },
+      variables () {
+        const authUser = this.$auth.user
+        const id = authUser[ 'https://hasura.io/jwt/claims' ][ 'x-hasura-user-id' ]
+        return { id }
+      }
+    }
   }
 }
 </script>
