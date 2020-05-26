ce9b90b442be09f2ae5b4cc85a031bfb83906dc4
Chris Rault <chris@smokeyfro.com>
2020-05-26 17:27:07 +0200

    Home hero tweaks

--- src/components/NewsletterForm.vue
+++ src/components/NewsletterForm.vue
@@ -1,6 +1,6 @@
 <template>
   <form
-    class="form newsletter mb-20 md:mb-0 mt-5 pt-10 mt-8 w-auto"
+    class="form newsletter mb-20 md:mb-0 w-auto"
     :class="align || 'text-center mx-auto'"
     @submit.prevent="subscribe">
     <h2 v-if="title != '' && headingLevel === '2'" class="text-gray-100 text-4xl font-bold mb-3">
@@ -9,10 +9,10 @@
     <h3 v-if="title != '' && headingLevel === '3'" class="text-gray-100 text-4xl font-bold mb-3">
       {{ title || 'Get notified when we launch' }}
     </h3>
-    <p v-if="desc != ''" class="text-gray-100 text-lg mb-6">
+    <p v-if="desc != ''" class="text-gray-100 text-2xl max-w-3xl mx-auto mb-10">
       {{ desc || 'We\'re still working on our initial products, but add your name and email to our list and we\'ll let you know when we go live.' }}
     </p>
-    <div class="flex flex-col md:flex-row md:items-center">
+    <div class="flex flex-col md:flex-row md:items-center md:justify-center">
       <div class="md:w-1/3 mb-4 md:mb-0 md:mr-5">
         <label
           for="name"
