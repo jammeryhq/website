2f706cbfc19b392f8d8bc1d0ca3c21aef13e020a
Chris Rault <chris@smokeyfro.com>
2020-06-04 14:29:53 +0200

    WIP survey page responsive

--- src/pages/2020-survey.vue
+++ src/pages/2020-survey.vue
@@ -1,21 +1,18 @@
 <template>
   <Layout>
-    <div class="flex items-start justify-between relative pt-10 h-full">
-      <div class="w-1/2 p-10 pt-0 survey-content">
-        <h1 class="title">
+    <div class="md:flex items-start justify-between relative lg:pt-10 h-full">
+      <div class="w-2/3 lg:w-1/2 p-10 pt-0 lg:fixed survey-content">
+        <h1 class="text-4xl sm:text-6xl font-bold">
           2020 Survey
         </h1>
-        <h2 class="text-5xl font-bold my-4">
+        <h2 class="text-2xl md:text-2xl lg:text-3xl xl:text-5xl font-bold my-4">
           Help us build products you'll love
         </h2>
-        <p class="excerpt">
+        <p class="text-lg md:text-xl lg:text-2xl xl:text-3xl">
           To ensure we're hyper focussed on building products and services that have an actual need, we're reaching out to the <strong>JAMstack Community</strong> to get feedback on areas that can be improved while building <em>Vue</em> and <em>Gridsome</em> based sites.
         </p>
       </div>
-      <g-image
-        src="/2.svg"
-        class="image" />
-      <div class="w-1/3 bg-white pr-20 shadow-2xl fixed right-0 h-full survey-form overflow-y-auto z-50">
+      <div class="w-auto lg:w-1/3 bg-white xl:pr-20 lg:shadow-2xl lg:fixed lg:right-0 lg:h-full survey-form overflow-y-auto z-50">
         <iframe
           title="Survey Form"
           src="https://docs.google.com/forms/d/e/1FAIpQLSfD11Wb4d4SLjUYoG3CcNimyIr0Qy75Y4zHZV1LbhNA3vyjBQ/viewform?embedded=true"
@@ -23,10 +20,12 @@
           height="5478"
           frameborder="0"
           marginheight="0"
-          marginwidth="0"
-          class="mr-10 text-xl">Loading…</iframe>
+          marginwidth="0">Loading…</iframe>
       </div>
     </div>
+          <g-image
+        src="/2.svg"
+        class="image" />
   </Layout>
 </template>
 
@@ -60,15 +59,12 @@ export default {
 </script>
 
 <style lang="scss">
-  .survey footer {
-    @apply hidden
-  }
+
   .survey {
       header {
-          @apply bg-white fixed;
+          @apply bg-white;
       }
     .survey-content {
-        @apply fixed;
         top: 170px;
     }
     .survey-form {
@@ -76,10 +72,47 @@ export default {
     }
   }
   .image {
-      position: absolute;
-      top: 200px;
-      width: 500px;
-      right: 430px;
-      z-index: 0;
+    position: absolute;
+    top: 150px;
+    width: 300px;
+    right: -80px;
+    z-index: 0;
   }
+
+body.survey {
+  @apply overflow-x-hidden
+}
+@media ( min-width: 1024px ) {
+    .survey {
+      header {
+          @apply fixed;
+      }
+    }
+}
+
+/* Small (sm) */
+@media (min-width: 640px) {
+  
+}
+
+/* Medium (md) */
+@media (min-width: 768px) {
+  
+}
+
+/* Large (lg) */
+@media (min-width: 1024px) {
+  .image {
+    position: fixed;
+    top: 200px;
+    width: 500px;
+    left: 50%;
+  }
+}
+
+/* Extra Large (xl) */
+@media (min-width: 1280px) {
+  
+}
+
 </style>
