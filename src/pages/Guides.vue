c464b9ca3bfda292da7aae0f2d3914af23252f0b
Chris Rault <chris@webmonkeys.co.za>
2020-07-29 09:38:24 +0200

    New guides popover with links wip

--- src/pages/Guides.vue
+++ src/pages/Guides.vue
@@ -6,61 +6,50 @@
         summary="Below are the setup guides for our Gridsome plugins and starters." />
       <div>
         <div class="grid gap-10 grid-cols-1 md:grid-cols-2 pb-20">
+          <!-- Plugins -->
           <div class="bg-gray-100 p-10 rounded-md hover:shadow-2xl">
             <h2>Plugins</h2>
             <p>Comprehensive setup guides for our Gridsome plugins.</p>
-            <div class="inline-block relative w-full">
-              <div class="relative">
-                <button
-                  v-if="isOpen"
-                  class="fixed inset-0 h-full w-full bg-black opacity-50 cursor-default"
-                  tabindex="-1"
-                  @click="isOpen = false"
-                  @keyup="isOpen = false" />
-                <div
-                  v-if="isOpen"
-                  class="absolute right-0 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl">
-                  <div
+            <div class="relative group appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+              <button @click="isOpen = !isOpen" class="w-full flex items-center justify-between cursor-pointer text-sm px-4 py-4">
+                Select a plugin guide
+                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+              </button>
+              <div v-if="isOpen" class="shadow-2xl border text-sm items-center absolute bottom-0 rounded-lg p-1 bg-white p-2 w-full">
+                <div class="grid grid-cols-1 md:grid-cols-2">
+                  <g-link
+
                     v-for="edge in $page.plugins.edges"
-                    :key="edge.node.id">
-                    <g-link
-                      :to="edge.node.path"
-                      class="text-black">
-                      {{ edge.node.title }}
-                    </g-link>
-                  </div>
+                    :key="edge.node.id"
+                    :to="edge.node.path"
+                    class="text-black">
+                    {{ edge.node.title }}
+                  </g-link>
+                  <button v-if="isOpen === true" @click="isOpen = !isOpen" class="absolute right-0 top-0 bg-black rounded-full h-6 w-6 text-white font-bold text-xs mr-3 mt-3">X</button>
                 </div>
-              </div>
-              <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg
-                  class="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
               </div>
             </div>
           </div>
+          <!-- Starters -->
           <div class="bg-gray-100 p-10 rounded-md hover:shadow-2xl">
-            <h2 class="mt-0">
-              Starters
-            </h2>
+            <h2>Starters</h2>
             <p>Comprehensive setup guides for our Gridsome starters.</p>
-            <div class="inline-block relative w-full">
-              <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-                <option value="">
-                  Select a starter guide
-                </option>
-                <option
-                  v-for="edge in $page.starters.edges"
-                  :key="edge.node.id"
-                  class="post">
-                  {{ edge.node.title }}
-                </option>
-              </select>
-              <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                <svg
-                  class="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+            <div class="relative group appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+              <button @click="isOpen = !isOpen" class="w-full flex items-center justify-between cursor-pointer text-sm px-4 py-4">
+                Select a starter guide
+                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+              </button>
+              <div v-if="isOpen" class="shadow-2xl border text-sm items-center absolute bottom-0 rounded-lg p-1 bg-white p-2 w-full">
+                <div class="grid grid-cols-1 md:grid-cols-2">
+                  <g-link
+                    v-for="edge in $page.starters.edges"
+                    :key="edge.node.id"
+                    :to="edge.node.path"
+                    class="text-black">
+                    {{ edge.node.title }}
+                  </g-link>
+                  <button v-if="isOpen === true" @click="isOpen = !isOpen" class="absolute right-0 top-0 bg-black rounded-full h-6 w-6 text-white font-bold text-xs mr-3 mt-3">X</button>
+                </div>
               </div>
             </div>
           </div>
@@ -106,7 +95,7 @@ export default {
       isOpen: false
     }
   },
-  created () {
+  mounted () {
     const handleEscape = e => {
       if (e.key === 'Esc' || e.key === 'Escape') {
         this.isOpen = false
