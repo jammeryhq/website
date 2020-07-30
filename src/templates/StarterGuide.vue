3a4b73c566eea1b085708c830413181c7526cdaa
Chris Rault <chris@webmonkeys.co.za>
2020-07-30 13:27:07 +0200

    WIP adding guide content

--- src/templates/StarterGuide.vue
+++ src/templates/StarterGuide.vue
@@ -2,25 +2,28 @@
   <Layout>
     <div class="p-6 lg:p-0 w-full mx-auto relative">
       <div class="prose lg:prose-2xl mx-auto">
-        <div class="">
-          <h1 class="title">
-            {{ $page.record.title }} Setup Guide
+        <div class="guide-content">
+          <h1 class="mb-0">
+            {{ $page.record.title }} Starter
           </h1>
-          <div class="flex items-start">
+          <h2>Setup Guide</h2>
+          <div class="flex items-start justify-between w-full">
             <p
               v-if="$page.record.excerpt"
-              class="excerpt w-3/4">
+              class="w-4/6 mt-0 excerpt">
                 {{ $page.record.excerpt }}
             </p>
-            <nav class="w-1/4 links">
-              <g-link :to="`${$page.record.path}`">
-                Plugin Details
-              </g-link>
-              <a :href="$page.record.demo + '.jammeryhq.com'">View Demo</a>
-              <a :href="'https://github.com/jammeryhq/' + $page.record.repo">Github Repo</a>
-            </nav>
+            <div class="w-2/6 relative">
+              <nav class="links relative z-20 flex flex-col -mt-1 items-center text-sm text-white font-bold">
+                <g-link :to="`${$page.record.path}`">Plugin Details</g-link>
+                <a :href="$page.record.demo">View Demo</a>
+                <a :href="'https://github.com/jammeryhq/' + $page.record.repo">Github Repo</a>
+                <g-link :to="`${$page.record.path}`">Release Notes</g-link>
+              </nav>
+              <Blob />
+            </div>
           </div>
-          <ul>
+          <ul class="text-lg">
             <li v-if="$page.record.version">
               Version: {{ $page.record.version }}
             </li>
@@ -34,6 +37,16 @@
     </div>
   </Layout>
 </template>
+
+<script>
+import Blob from '@/components/Blob'
+
+export default {
+  components: {
+    Blob
+  },
+}
+</script>
 
 <page-query>
 query($id:ID) {
@@ -58,7 +71,13 @@ query($id:ID) {
 .prose .excerpt {
   @apply text-3xl mt-0
 }
+.guide-content > h1 {
+  @apply mb-0;
+}
+.guide-content > h2 {
+  @apply mt-0;
+}
 .links a {
-  @apply block text-lg
+  @apply block text-sm text-white
 }
 </style>
