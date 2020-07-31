28fd1caca290ece6615cb545f87e01f04914da99
Travis Reynolds <travis@cirruscreative.co.uk>
2020-07-31 21:52:51 +0000

    Update handleEscape methods

--- src/templates/Starter.vue
+++ src/templates/Starter.vue
@@ -6,75 +6,93 @@
         subtitle="Starter for Gridsome"
         :summary="$page.starter.excerpt"
         custom-class="prose lg:prose-2xl mx-auto pb-0">
-          <template v-slot:page-links>
-            <div
-              v-if="$page.starter.availability != 5"
-              class="mb-8 flex items-center justify-start mt-10 space-x-8 text-center">
+        <template v-slot:page-links>
+          <div
+            v-if="$page.starter.availability != 5"
+            class="mb-8 flex items-center justify-start mt-10 space-x-8 text-center">
+            <button
+              class="button button--small button-secondary focus:outline-none focus:shadow-outline focus:border-yellow-500"
+              title="Show install instructions"
+              @click="showInstall = !showInstall"
+              @keyup="showInstall = !showInstall">
+              Install
+            </button>
+            <a
+              :href="$page.starter.demo"
+              class="text-black text-2xl"
+              title="View the demob">
+              Demo
+            </a>
+            <a
+              :href="repoUrl"
+              class="text-black text-2xl"
+              title="View the repo on Github">
+              Github
+            </a>
+            <span class="text-gray-400">|</span>
+            <span><strong>v{{ $page.starter.version }}</strong> (<g-link
+              :to="releaseNotesUrl"
+              class="p-0 underline">Release Notes</g-link>)</span>
+          </div>
+          <div
+            v-if="showInstall"
+            v-on-clickaway="hideInstall"
+            class="absolute z-50 -mt-3 bg-yellow-200 p-8 shadow-2xl border-yellow-400 rounded-md w-1/2">
+            <p class="text-xl mb-5 pr-20">
+              To install <strong>{{ $page.starter.title }}</strong> using Yarn or NPM, simply copy the relevant snippet and paste it into your terminal.
+            </p>
+            <div class="border rounded-md bg-white px-3 py-3 w-full flex items-center justify-between">
+              <span class="flex h-8 text-sm">
+                <button
+                  :class="{'bg-green-500':installType === 'yarn'}"
+                  class="font-bold bg-gray-900 text-white rounded-md rounded-r-none px-2"
+                  @click="installType = 'yarn'"
+                  @keyup="installType = 'yarn'">Yarn</button>
+                <button
+                  :class="{'bg-green-500':installType === 'npm'}"
+                  class="font-bold bg-gray-900 text-white rounded-md rounded-l-none px-2"
+                  @click="installType = 'npm'"
+                  @keyup="installType = 'npm'">NPM</button>
+              </span>
+              <input
+                id="install-code"
+                :value="installText"
+                name="install-code"
+                type="text"
+                disabled
+                class="shadow-inner bg-gray-100 h-8 px-2 mx-3 font-mono rounded-md text-base w-full flex-grow">
               <button
-                class="button button--small button-secondary focus:outline-none focus:shadow-outline focus:border-yellow-500"
-                title="Show install instructions"
-                @click="showInstall = !showInstall"
-                @keyup="showInstall = !showInstall">
-                Install
-              </button>
-              <a
-                :href="$page.starter.demo"
-                class="text-black text-2xl"
-                title="View the demob">
-                Demo
-              </a>
-              <a
-                :href="repoUrl"
-                class="text-black text-2xl"
-                title="View the repo on Github">
-                Github
-              </a>
-              <span class="text-gray-400">|</span>
-              <span><strong>v{{ $page.starter.version }}</strong> (<g-link :to="releaseNotesUrl" class="p-0 underline">Release Notes</g-link>)</span>
-            </div>
-            <div
-              v-if="showInstall"
-              v-on-clickaway="hideInstall"
-              class="absolute z-50 -mt-3 bg-yellow-200 p-8 shadow-2xl border-yellow-400 rounded-md w-1/2">
-              <p class="text-xl mb-5 pr-20">To install <strong>{{ $page.starter.title }}</strong> using Yarn or NPM, simply copy the relevant snippet and paste it into your terminal.</p>
-              <div class="border rounded-md bg-white px-3 py-3 w-full flex items-center justify-between">
-                <span class="flex h-8 text-sm">
-                  <button
-                    :class="{'bg-green-500':installType === 'yarn'}"
-                    class="font-bold bg-gray-900 text-white rounded-md rounded-r-none px-2"
-                    @click="installType = 'yarn'"
-                    @keyup="installType = 'yarn'">Yarn</button>
-                  <button
-                    :class="{'bg-green-500':installType === 'npm'}"
-                    class="font-bold bg-gray-900 text-white rounded-md rounded-l-none px-2"
-                    @click="installType = 'npm'"
-                    @keyup="installType = 'npm'">NPM</button>
-                </span>
-                <input
-                  id="install-code"
-                  :value="installText"
-                  name="install-code"
-                  type="text"
-                  disabled
-                  class="shadow-inner bg-gray-100 h-8 px-2 mx-3 font-mono rounded-md text-base w-full flex-grow">
-                <button 
-                  class="bg-gray-900 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded-full inline-flex h-8 text-sm leading-none items-center"
-                  v-clipboard:copy="installText"
-                  title="Copy to your clipboard">
-                  <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>
-                  <span>Copy</span>
-                </button>
-              </div>
-              <button
-                class="absolute top-0 right-0 mt-4 mr-4 bg-red-600 text-white inline-flex items-center justify-center w-8 h-8 text-center rounded-full font-bold"
-                @click.prevent="showInstall = !showInstall"
-                @keyup="showInstall = !showInstall">
-                &times;
+                v-clipboard:copy="installText"
+                class="bg-gray-900 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded-full inline-flex h-8 text-sm leading-none items-center"
+                title="Copy to your clipboard">
+                <svg
+                  class="w-4 h-4 mr-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="#fff"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"><rect
+                    x="9"
+                    y="9"
+                    width="13"
+                    height="13"
+                    rx="2"
+                    ry="2" /><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" /></svg>
+                <span>Copy</span>
               </button>
             </div>
-          </template>
+            <button
+              class="absolute top-0 right-0 mt-4 mr-4 bg-red-600 text-white inline-flex items-center justify-center w-8 h-8 text-center rounded-full font-bold"
+              @click.prevent="showInstall = !showInstall"
+              @keyup="showInstall = !showInstall">
+              &times;
+            </button>
+          </div>
+        </template>
       </PageHeader>
-      
+
       <figure class="block border rounded-md overflow-hidden mt-10 mb-20 mx-20">
         <g-image :src="$page.starter.image" />
       </figure>
@@ -93,6 +111,11 @@ export default {
   components: {
     PageHeader
   },
+  mixins: [clickaway],
+  data: () => ({
+    showInstall: false,
+    installType: 'yarn'
+  }),
   computed: {
     installText () {
       const repo = this.$page.starter.repo
@@ -105,13 +128,9 @@ export default {
       return `${githubUrl}${repoName}`
     },
     releaseNotesUrl () {
-      return '/starters/'+this.$page.starter.slug+'/release-notes'
+      return '/starters/' + this.$page.starter.slug + '/release-notes'
     }
   },
-  data: () => ({
-    showInstall: false,
-    installType: 'yarn'
-  }),
   metaInfo () {
     return {
       bodyAttrs: {
@@ -119,22 +138,19 @@ export default {
       }
     }
   },
-  methods: {
-    hideInstall() {
-        this.showInstall = false
-    }
-  },
-  mixins: [clickaway],
   mounted () {
-      const handleEscape = e => {
-        if (e.key === 'Esc' || e.key === 'Escape') {
-            this.showInstall
-        }
-      }
-      document.addEventListener('keydown', handleEscape)
-      this.$once('hook:afterDestroy', () => {
-        document.removeEventListener('keydown', handleEscape)
-      })
+    const handleEscape = e => {
+      if (e.code === 'Escape') this.hideInstall()
+    }
+    document.addEventListener('keyup', handleEscape)
+    this.$once('hook:afterDestroy', () => {
+      document.removeEventListener('keyup', handleEscape)
+    })
+  },
+  methods: {
+    hideInstall () {
+      this.showInstall = false
+    }
   }
 }
 </script>
