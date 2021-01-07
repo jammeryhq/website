e88bde07adb1535b4b7345a310776d5806184d78
Chris Rault <chris@smokeyfro.com>
2021-01-07 11:24:52 +0200

    Blog index tweaks

--- src/pages/blog/Index.vue
+++ src/pages/blog/Index.vue
@@ -1,20 +1,16 @@
 <template>
   <Layout>
-    <div class="min-h-screen p-6 md:p-0">
+    <div class="p-6 md:p-0 container mx-auto">
       <PageHeader
         title="Updates from HQ"
         summary="Where we post updates about what we're working on, new product launches and random thoughts on the JAMstack." />
-      <div class="prose lg:prose-2xl mx-auto mt-10">
+      <div class="p-6 md:p-0 mt-10 mb-20 lg:w-1/2 mx-auto">
         <article
           v-for="edge in $page.blog.edges"
-          :key="edge.node.id"
-          class="post">
-          <h2
-            class="text-2xl lg:text-4xl font-bold bg-black inline text-white p-3 mb-0"
-            :class="`${edge.node.topic}`">
+          :key="edge.node.id">
+          <h2>
             <g-link
-              :to="`${edge.node.path}`"
-              class="text-white">
+              :to="`${edge.node.path}`">
               {{ edge.node.title }}
             </g-link>
           </h2>
@@ -61,16 +57,4 @@ query Blog ($page: Int) {
 }
 </page-query>
 
-<style>
-  .blog {
-    .post {
-      @apply pb-0;
-      h2 {
-        @apply rounded-md mt-6;
-        a {
-          @apply text-white no-underline;
-        }
-      }
-    }
-  }
-</style>
+
